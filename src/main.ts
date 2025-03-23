a172bc3379975fb2a302647cfec1fbea607282f8
long.nguyen <long29103107@gmail.com>
2025-03-23 14:28:48 +0700

    loaded antdesign

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,13 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import '@/styles/main.css'
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/reset.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(Antd)
 app.use(router)
 
 app.mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,5 @@
 <template>
   <main>Hello Vue</main>
   <div class="text-red-500 text-2xl font-bold">Hello Tailwind CSS!</div>
+  <a-button type="primary">Click me</a-button>
 </template>
