420fcb1dd72301aa18567fecc16acc35aa662d8b
long.nguyen <long29103107@gmail.com>
2025-04-01 17:26:21 +0700

    No Message

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -11,6 +11,7 @@ import { GoogleLogin } from 'vue3-google-login'
 import axios from 'axios'
 import { exceptionHandler } from '@/utils/exceptionHandler'
 import { useRouter } from 'vue-router'
+import URL_CONSTANTS from '@/constants/url-constants'
 
 const router = useRouter()
 
@@ -19,7 +20,7 @@ const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID || ''
 const handleLoginSuccess = async (response) => {
   try {
     const res = await axios.post(
-      `${import.meta.env.VITE_API_URL}/auth/google-login`,
+      `${import.meta.env.VITE_API_URL}${URL_CONSTANTS.AUTH.LOGIN_GOOGLE}`,
       { token: response.credential },
       { withCredentials: true },
     )
